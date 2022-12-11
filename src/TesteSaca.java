534c9defc06aeabfc58a597c68e5b71eb6b27e0e
leandromenoci <menoci.leandro@gmail.com>
2022-12-11 18:04:20 -0300

    first commit

---
+++ src/TesteSaca.java
@@ -0,0 +1,14 @@
+public class TesteSaca {
+  public static void main(String[] args) {
+    Conta conta = new ContaCorrente(123, 3121);
+
+    conta.deposita(200.0);
+    try {
+      conta.saca(210.0);
+    } catch (Exception ex) {
+      System.out.println("Ex: " + ex.getMessage());
+    }
+
+    System.out.println(conta.getSaldo());
+  }
+}
