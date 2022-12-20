eda35851e4c6f7c0c45592a35f753ca83aaeda52
leandromenoci <menoci.leandro@gmail.com>
2022-12-20 09:12:49 -0300

    Java.lang / toString()

---
+++ src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,37 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+  public static void main(String[] args) {
+
+    String nome = "Alura"; // object literal (imutável)
+    // String outro = new String("Alura");
+
+    System.out.println(nome.contains("Alu"));
+
+    // String vazio = " ";
+    // String outroVazio = vazio.trim();
+    // System.out.println(outroVazio.isEmpty());
+
+    // System.out.println(nome.length());
+    // for (int i = 0; i < nome.length(); i++) {
+    // System.out.println(nome.charAt(i));
+    // }
+
+    // String sub = nome.substring(1);
+    // System.out.println(sub);
+
+    // int pos = nome.indexOf("ur");
+    // System.out.println(pos);
+
+    // char c = nome.charAt(2);
+    // System.out.println(c);
+
+    // char c = 'a';
+    // char d = 'A';
+    // String outra = nome.replace(d, c);
+    // String outra = nome.replace("A", "a");
+
+    // System.out.println(nome);
+    // System.out.println(outra);
+  }
+}
